91480324e1c5b900431bd58cb52ee1be02d860a3
NZhukov <nzhukov.rnd@gmail.com>
2023-03-17 20:37:46 +0400

    Initial Commit

---
+++ NamesCompApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamesCompApp
+//
+//  Created by Nikolay Zhukov on 17.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
