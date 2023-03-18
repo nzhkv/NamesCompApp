02838b52eb6ab110f730571ffb24d346915d7388
NZhukov <nzhukov.rnd@gmail.com>
2023-03-18 17:16:11 +0400

    save code

--- NamesCompApp/FirstViewController.swift
+++ NamesCompApp/FirstViewController.swift
@@ -70,3 +70,19 @@ class FirstViewController: UIViewController {
     
 }
 
+extension FirstViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == yourNameTF {
+            partnerNameTF.becomeFirstResponder()
+        } else {
+            resultButtonTapped()
+        }
+        return true
+    }
+}
+
--- NamesCompApp/ResultViewController.swift
+++ NamesCompApp/ResultViewController.swift
@@ -16,18 +16,73 @@ class ResultViewController: UIViewController {
     
     @IBOutlet weak var backButton: UIButton!
     
-    
     var firstName: String!
     var secondName: String!
     
+    private var resultValie = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         backButton.layer.borderWidth = 2
         backButton.layer.borderColor = UIColor.systemGroupedBackground.cgColor
-        backButton.layer.cornerRadius = 10
+        
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 6)
         
         namesLabel.text = "\(firstName ?? "") and \(secondName ?? "")"
+        
+        resultValie = findResult()
+        resultLabel.text = resultValie.formatted(.percent)
+        
+        progressView.progress = Float(resultValie) / 100
+        
     }
-
+    
+    private func findValue( for name: String) -> Int {
+        var count = 0
+        for character in name.lowercased () {
+            switch character {
+            case "a", "i", "j", "q", "y":
+                count += 1
+            case "b", "k", "r":
+                count += 2
+            case "c", "g", "l", "s":
+                count += 3
+            case "d", "m", "t":
+                count += 4
+            case "e", "h", "n", "x":
+                count += 5
+            case "u", "v", "w":
+                count += 6
+            case "o", "z":
+                count += 7
+            case "f", "p":
+                count += 8
+            default:
+                count += 0
+            }
+        }
+            return count
+    }
+    
+    private func findResult() -> Int {
+        var result = 0
+        let firstValue = findValue(for: firstName)
+        let secondValue = findValue (for: secondName)
+        let absDifference = abs(firstValue - secondValue)
+        
+        switch absDifference {
+        case 0, 1, 2:
+            result = 100
+        case 3, 4:
+            result = 65
+        case 5:
+            result = 50
+        case 6:
+            result = 40
+        default:
+            result = 30
+        }
+        return result
+    }
+    
 }
